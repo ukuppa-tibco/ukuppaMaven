ac2faec2467e8ea5d5da069def5fe0e9476fe4bd
Usha Kuppa <ukuppa@tibco.com>
2022-05-26 12:35:34 +0530

    Add files via upload
    Checking in sample maven project

---
+++ UnitTestDemoProject/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/UnitTestDemoProject/Tests/ActivityAssertionWithGoldFile.bwt#/"/>
+  <EmulationDataFile>ACED000574002853756250726F63657373576974685072696D6974697665417373657274696F6E546573742E627774</EmulationDataFile>
+  <EmulationDataFile>ACED000574002753756250726F63657373576974684163746976697479417373657274696F6E546573742E627774</EmulationDataFile>
+  <EmulationDataFile>ACED00057400214163746976697479417373657274696F6E57697468476F6C6446696C652E627774</EmulationDataFile>
+  <EmulationDataFile>ACED0005740018556E69745465737444656D6F50726F6A656374312E627774</EmulationDataFile>
+</emulation:BWTFile>
